d1f264f04f25226569b7bb4e1727fc4163722828
eontool <eontool@gmail.com>
2013-09-02 17:35:18 -0700

    Site updated at 2013-09-03 00:35:18 UTC

---
+++ downloads/code/demo_impl.cc
@@ -0,0 +1,80 @@
+/* -*- c++ -*- */
+/* 
+ * Copyright 2013 <+YOU OR YOUR COMPANY+>.
+ * 
+ * This is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3, or (at your option)
+ * any later version.
+ * 
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this software; see the file COPYING.  If not, write to
+ * the Free Software Foundation, Inc., 51 Franklin Street,
+ * Boston, MA 02110-1301, USA.
+ */
+
+#ifdef HAVE_CONFIG_H
+#include "config.h"
+#endif
+
+#include <gnuradio/io_signature.h>
+#include "demo_impl.h"
+
+namespace gr {
+  namespace pruebas {
+
+    demo::sptr
+    demo::make()
+    {
+      return gnuradio::get_initial_sptr
+        (new demo_impl());
+    }
+
+    /*
+     * The private constructor
+     */
+    demo_impl::demo_impl()
+      : gr::block("demo",
+              gr::io_signature::make(0, 0, sizeof(int)),
+              gr::io_signature::make(1, 1, sizeof(int)))
+    {}
+
+    /*
+     * Our virtual destructor.
+     */
+    demo_impl::~demo_impl()
+    {
+    }
+
+    void
+    demo_impl::forecast (int noutput_items, gr_vector_int &ninput_items_required)
+    {
+        /* <+forecast+> e.g. ninput_items_required[0] = noutput_items */
+    }
+
+    int
+    demo_impl::general_work (int noutput_items,
+                       gr_vector_int &ninput_items,
+                       gr_vector_const_void_star &input_items,
+                       gr_vector_void_star &output_items)
+    {
+        const int *in = (const int *) input_items[0];
+        int *out = (int *) output_items[0];
+
+        // Do <+signal processing+>
+        // Tell runtime system how many input items we consumed on
+        // each input stream.
+        consume_each (noutput_items);
+
+        // Tell runtime system how many output items we produced.
+        return noutput_items;
+    }
+
+  } /* namespace pruebas */
+} /* namespace gr */
+
